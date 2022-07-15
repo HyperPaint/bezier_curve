1b407ee6aa9e1d29362086f7dd8bfa8bcf25017a
HyperPaint <dioman172@yandex.ru>
2022-07-15 20:10:45 +1000

    Initial commit

---
+++ bezier_curve/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Picture = new System.Windows.Forms.PictureBox();
+            this.ButtonPaint = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Picture
+            // 
+            this.Picture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Picture.Location = new System.Drawing.Point(12, 12);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(776, 397);
+            this.Picture.TabIndex = 0;
+            this.Picture.TabStop = false;
+            // 
+            // ButtonPaint
+            // 
+            this.ButtonPaint.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonPaint.Location = new System.Drawing.Point(12, 415);
+            this.ButtonPaint.Name = "ButtonPaint";
+            this.ButtonPaint.Size = new System.Drawing.Size(776, 23);
+            this.ButtonPaint.TabIndex = 1;
+            this.ButtonPaint.Text = "Р и с о в а т ь";
+            this.ButtonPaint.UseVisualStyleBackColor = true;
+            this.ButtonPaint.Click += new System.EventHandler(this.ButtonPaint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonPaint);
+            this.Controls.Add(this.Picture);
+            this.Name = "Form1";
+            this.Text = "Форма";
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Button ButtonPaint;
+    }
+}
+
---
+++ bezier_curve/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPaint_Click(object sender, EventArgs e)
+        {
+            Picture.Image = Bezier.DrawRandom(Picture.Width, Picture.Height);
+        }
+    }
+
+    public static class Bezier
+    {
+        private struct Point
+        {
+            public int x, y;
+        };
+
+        private struct Line
+        {
+            public int x1, y1, x2, y2;
+        };
+
+        public static Bitmap DrawRandom(int width, int height)
+        {
+            Bitmap bitmap = new Bitmap(width, height);
+            Color colorMain = Color.Black;
+            Color colorSecondary = Color.Red;
+            // количество точек
+            const int PointsMin = 3;
+            const int PointsMax = 15;
+            // получение списка точек
+            Random r = new Random();
+            int PointsCount = r.Next(PointsMin, PointsMax);
+            Point[] Points = new Point[PointsCount];
+            for (int i = 0; i < PointsCount; i++)
+            {
+                Points[i].x = r.Next(width);
+                Points[i].y = r.Next(height);
+            }
+            // получение списка точек кривой
+            float t = 0.0f;
+            const float delta_t = 0.01f;
+            const int iterations = (int)(1 / delta_t);
+            List<Point> list = new List<Point>();
+            list.Add(Points[0]);
+            for (int i = 0; i < iterations; i++)
+            {
+                t += delta_t;
+                list.Add(GetPoint(Points, PointsCount, t));
+            }
+            list.Add(Points[PointsCount - 1]);
+            try
+            {
+                //отрисовка кривой
+                for (int i = 0; i < list.Count - 1; i++)
+                {
+                    Brezenham.Line(list[i].x, list[i].y, list[i + 1].x, list[i + 1].y, ref bitmap);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+            // отрисовка точек
+            for (int i = 0; i < PointsCount; i++)
+            {
+                bitmap.SetPixel(Points[i].x, Points[i].y, colorSecondary);
+            }
+
+            return bitmap;
+        }
+
+        // t = [a, b]
+        private static Point GetPoint(Point[] Points, int PointsCount, float t)
+        {
+            Point point = new Point();
+            Line line = GetLine(Points, PointsCount, t);
+            Point vector = GetLineVector(line, t);
+            point.x = line.x1 + vector.x;
+            point.y = line.y1 + vector.y;
+            return point;
+        }
+
+        private static Line GetLine(Point[] Points, int PointsCount, float t)
+        {
+            if (PointsCount > 2)
+            {
+                Point[] points = new Point[PointsCount - 1];
+                Line line = new Line();
+                Point vector;
+                for (int i = 0; i < PointsCount - 1; i++)
+                {
+                    line.x1 = Points[i].x; 
+                    line.x2 = Points[i + 1].x;
+                    line.y1 = Points[i].y; 
+                    line.y2 = Points[i + 1].y;
+                    vector = GetLineVector(line, t);
+                    points[i].x = Points[i].x + vector.x;
+                    points[i].y = Points[i].y + vector.y;
+                }
+                return GetLine(points, PointsCount - 1, t);
+            }
+            else if (PointsCount == 2)
+            {
+                Line line = new Line();
+                line.x1 = Points[0].x;
+                line.y1 = Points[0].y;
+                line.x2 = Points[1].x;
+                line.y2 = Points[1].y;
+                return line;
+            }
+            else return new Line();
+        }
+
+        private static Point GetLineVector(Line line, float t)
+        {
+            Point length = new Point();
+            length.x = (int)((line.x2 - line.x1) * t);
+            length.y = (int)((line.y2 - line.y1) * t);
+            return length;
+        }
+    }
+
+    public static class Brezenham
+    {
+        public static void Line(int x1, int y1, int x2, int y2, ref Bitmap bitmap)
+        {
+            // проверяем рост отрезка по оси x и по оси y
+            var steep = Math.Abs(y2 - y1) > Math.Abs(x2 - x1);
+            // отражаем линию по диагонали, если угол наклона слишком большой
+            if (steep)
+            {
+                Swap(ref x1, ref y1);
+                Swap(ref x2, ref y2);
+            }
+            // если линия растёт не слева направо, то меняем начало и конец отрезка местами
+            if (x1 > x2)
+            {
+                Swap(ref x1, ref x2);
+                Swap(ref y1, ref y2);
+            }
+            int dx = x2 - x1;
+            int dy = Math.Abs(y2 - y1);
+            int error = dx / 2;
+            int ystep = (y1 < y2) ? 1 : -1;
+            int y = y1;
+            for (int x = x1; x <= x2; x++)
+            {
+                bitmap.SetPixel(steep ? y : x, steep ? x : y, Color.Black);
+                error -= dy;
+                if (error < 0)
+                {
+                    y += ystep;
+                    error += dx;
+                }
+            }
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+    }
+}
